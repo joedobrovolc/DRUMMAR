2826160a6eeac84c091215ff6dac2e00eb7c2932
joedobrovolc <dobrovolc.2@wright.edu>
2018-04-29 11:33:22 -0400

    Proof of Concept ready

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+    public Transform[] spawnpoints;
+    public GameObject cubePrefab;
+    private float timeToDrum = 2f;
+    public float timeBetweenSpawns = 1f;
+    int drum;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Instantiate(cubePrefab, spawnpoints[0]);
+        //Instantiate(cubePrefab, spawnpoints[1]);
+        drum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeToDrum)
+        {
+            timeToDrum = Time.time + timeBetweenSpawns;
+            if (drum == 0)
+            {
+                Instantiate(cubePrefab, spawnpoints[1].position, Quaternion.identity);
+                drum++;
+            }
+            else
+            {
+                Instantiate(cubePrefab, spawnpoints[0].position, Quaternion.identity);
+                drum--;
+            }
+        }
+
+    }
+
+}
+
+
